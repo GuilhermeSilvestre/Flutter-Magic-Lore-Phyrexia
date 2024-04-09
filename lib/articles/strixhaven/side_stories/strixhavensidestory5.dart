0b483f422b37be946939674e567490f7e59704b8
Guilherme Silvestre <gaasilvestre@gmail.com>
2024-04-09 00:54:37 -0300

    Deixando Strixhaven com dark mode

--- lib/articles/strixhaven/side_stories/strixhavensidestory5.dart
+++ lib/articles/strixhaven/side_stories/strixhavensidestory5.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:magic_lore/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 const textArticle = TextStyle(
   fontSize: 18,
@@ -10,8 +12,12 @@ class StrixhavenSideStory5 extends StatelessWidget {
   const StrixhavenSideStory5({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    bool darkModeEnabled = context.watch<ThemeProvider>().darkmode;
+
     return Scaffold(
+      backgroundColor: darkModeEnabled ? Colors.black : Colors.white,
       appBar: AppBar(
+        backgroundColor: darkModeEnabled ? Colors.grey : Colors.white,
         title: const Text(
           'STRIXHAVEN',
           style: TextStyle(
